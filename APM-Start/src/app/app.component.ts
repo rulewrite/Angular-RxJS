321943127e0db2c7f77951b8ae9ee42b7f881e63
rulewrite <rulewrite@gmail.com>
2020-12-09 23:41:24 +0900

    feat: 옵저버블 직접 생성하기 예제

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pageTitle = 'Acme Product Management';
+
+  ngOnInit(): void {
+    of(2, 4, 6, 8).subscribe(console.log);
+
+    from([2, 4, 6, 8]).subscribe(
+      (element) => console.log(`result: .. ${element}`),
+      (error) => console.error(`error: ${error}`),
+      () => console.log(`completed`)
+    );
+
+    of(...['Apple 1', 'Apple 2', 'Apple 3']).subscribe(
+      (apple) => console.log(`Apple was emitted ${apple}`),
+      (error) => console.error(`Error: ${error}`),
+      () => console.log(`No more apples.`)
+    );
+  }
 }
