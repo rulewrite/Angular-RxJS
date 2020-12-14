1b47df60560c0d2786b6c66257b4fbd9dc1a2180
rulewrite <rulewrite@gmail.com>
2020-12-15 00:21:39 +0900

    feat: rxjs operator map, tap, take 테스트

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of, from } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'pm-root',
@@ -12,11 +13,25 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     of(2, 4, 6, 8).subscribe(console.log);
 
-    from([2, 4, 6, 8]).subscribe(
-      (element) => console.log(`result: .. ${element}`),
-      (error) => console.error(`error: ${error}`),
-      () => console.log(`completed`)
-    );
+    from([20, 15, 10, 5])
+      .pipe(
+        tap((item) => console.log(`side effect ${item}.`)),
+        map((item) => item * 2),
+        map((item) => item - 10),
+        map((item) => {
+          if (item < 1) {
+            throw new Error('양수가 아니면 에러');
+          }
+          return item;
+        }),
+        // 방출 값 3개 까지만 구독
+        take(3)
+      )
+      .subscribe(
+        (element) => console.log(`result: .. ${element}`),
+        (error) => console.error(`error: ${error}`),
+        () => console.log(`completed`)
+      );
 
     of(...['Apple 1', 'Apple 2', 'Apple 3']).subscribe(
       (apple) => console.log(`Apple was emitted ${apple}`),
@@ -26,4 +41,4 @@ export class AppComponent implements OnInit {
   }
 }
 
-// 3강 완료
+// 4강 완료
