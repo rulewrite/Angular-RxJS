6eb7c123ec7e96a45ec1817786bb752285238940
rulewrite <rulewrite@gmail.com>
2020-12-25 17:44:04 +0900

    feat: 단일 상품 옵저버블 추가 및 바인딩

--- APM-Start/src/app/products/product-list-alt/product-detail.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent {
   pageTitle = 'Product Detail';
   errorMessage = '';
-  product;
+
+  product$ = this.productService.selectedProduct$.pipe(
+    catchError((error) => {
+      this.errorMessage = error;
+      return EMPTY;
+    })
+  );
 
   constructor(private productService: ProductService) {}
 }
