fa19d23bbb086a3532900eb4a3cde385855e1f38
rulewrite <rulewrite@gmail.com>
2020-12-25 12:43:08 +0900

    feat: 임시 필터링된 상품 적용

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { ProductService } from './product.service';
 
@@ -12,12 +12,25 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategory$.pipe(
     catchError((error: string) => {
       this.errorMessage = error;
       return EMPTY;
     })
+  );
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+    map((propducts) =>
+      propducts.filter((product) => {
+        if (!this.selectedCategoryId) {
+          return true;
+        }
+
+        return this.selectedCategoryId === product.categoryId;
+      })
+    )
   );
 
   constructor(private productService: ProductService) {}
