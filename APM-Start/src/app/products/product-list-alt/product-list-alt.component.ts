c68082a01dcb0de90009f93127e56305efd0a2ff
rulewrite <rulewrite@gmail.com>
2020-12-25 17:55:04 +0900

    feat: 상품 선택 액션 정의 및 바인딩

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -8,11 +8,11 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list-alt.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
-  selectedProductId: number;
 
   products$ = this.productService.productsWithCategory$.pipe(
     catchError((error: string) => {
@@ -21,9 +21,11 @@ export class ProductListAltComponent {
     })
   );
 
+  selectedProduct$ = this.productService.selectedProduct$;
+
   constructor(private productService: ProductService) {}
 
   onSelected(productId: number): void {
-    console.log('Not yet implemented');
+    this.productService.selectedProductChanged(productId);
   }
 }
