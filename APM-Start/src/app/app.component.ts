af34a860eeb25a19df0fe9dcf58e7530a8e5d2a6
rulewrite <rulewrite@gmail.com>
2020-12-25 18:38:09 +0900

    chore: 진도 표시 및 초기 테스트 코드 제거

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -10,35 +10,7 @@ import { map, take, tap } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   pageTitle = 'Acme Product Management';
 
-  ngOnInit(): void {
-    of(2, 4, 6, 8).subscribe(console.log);
-
-    from([20, 15, 10, 5])
-      .pipe(
-        tap((item) => console.log(`side effect ${item}.`)),
-        map((item) => item * 2),
-        map((item) => item - 10),
-        map((item) => {
-          if (item < 1) {
-            throw new Error('양수가 아니면 에러');
-          }
-          return item;
-        }),
-        // 방출 값 3개 까지만 구독
-        take(3)
-      )
-      .subscribe(
-        (element) => console.log(`result: .. ${element}`),
-        (error) => console.error(`error: ${error}`),
-        () => console.log(`completed`)
-      );
-
-    of(...['Apple 1', 'Apple 2', 'Apple 3']).subscribe(
-      (apple) => console.log(`Apple was emitted ${apple}`),
-      (error) => console.error(`Error: ${error}`),
-      () => console.log(`No more apples.`)
-    );
-  }
+  ngOnInit(): void {}
 }
 
-// 8강 완료
+// 9강 완료
