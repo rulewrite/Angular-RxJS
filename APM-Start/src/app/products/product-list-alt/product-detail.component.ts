79d509f4ec56f10579fc0e5051f9095ce692323b
rulewrite <rulewrite@gmail.com>
2020-12-25 17:40:10 +0900

    chore: 코드 컨벤션

--- APM-Start/src/app/products/product-list-alt/product-detail.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-detail.component.ts
@@ -4,13 +4,12 @@ import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-detail',
-  templateUrl: './product-detail.component.html'
+  templateUrl: './product-detail.component.html',
 })
 export class ProductDetailComponent {
   pageTitle = 'Product Detail';
   errorMessage = '';
   product;
 
-  constructor(private productService: ProductService) { }
-
+  constructor(private productService: ProductService) {}
 }
